d5fc71b1f01217938fd488e27f31346c4ada17dd
Marko Jovetic <marko140397@gmail.com>
2019-06-09 22:51:44 +0200

    Added different movie descriptions for all films and links for them in home and all movies

---
+++ ICR/js/fullMovieList.js
@@ -0,0 +1,125 @@
+var fullMovieList=[
+    {   
+        id:"jw3",
+        title:"John Wick 3",
+        released:"15.05.",
+        lenght: "2h 11m",
+        rating: "R",
+        criticScore: "9/10",
+        description:`In this third installment of the adrenaline-fueled action franchise,
+                        super-assassin John Wick (Reeves) 
+                        returns with a $14 million price tag on his head and an army of bounty-hunting killers on his trail. 
+                        After killing a member of the shadowy international assassin's guild,
+                        the High Table, John Wick is excommunicado, 
+                        but the world's most ruthless hit men and women await his every turn.`,
+        poster:"images/JohnWickPoster.jpeg",
+        rtLink:"https://www.rottentomatoes.com/m/john_wick_chapter_3_parabellum",
+        jumboPoster:"images/JW3Jumbo3.jpg",
+        trailer:"https://www.youtube.com/embed/pU8-7BX9uxs"
+    },
+    {   
+        id:"sw",
+        title:"Star Wars",
+        released:"25.05.",
+        lenght: "2h 15m",
+        rating: "PG-13",
+        criticScore: "10/10",
+        description:`Nineteen years after the formation of the Empire,
+         Luke Skywalker is thrust into the struggle of the Rebel Alliance when he meets Obi-Wan Kenobi,
+          who has lived for years in seclusion on the desert planet of Tatooine.
+         Obi-Wan begins Luke's Jedi training as Luke joins him on a daring mission to rescue the beautiful Rebel leader Princess Leia
+          from the clutches of the evil Empire`,
+        poster:"images/StarWarsPosterR.jpeg",
+        rtLink:"https://www.rottentomatoes.com/m/star_wars",
+        jumboPoster:"images/StarWarsPosterJumbo.jpg",
+        trailer:"https://www.youtube.com/embed/vZ734NWnAHA"
+    },
+    {   
+        id:"hm",
+        title:"Hotel Mumbai",
+        released:"25.05.",
+        lenght: "2h 0m",
+        rating: "R",
+        criticScore: "7/10",
+        description:`A gripping true story of humanity and heroism,
+         HOTEL MUMBAI vividly recounts the 2008 siege of the famed Taj Hotel by a group of terrorists in Mumbai, 
+         India. Among the dedicated hotel staff is the renowned chef Hemant Oberoi (Anupam Kher) and a waiter
+         (Academy Award-Nominee Dev Patel, Slumdog Millionaire) who choose to risk their lives to protect their guests`,
+        poster:"images/HotelMumbaiPoster.jpeg",
+        rtLink:"https://www.rottentomatoes.com/m/hotel_mumbai",
+        jumboPoster:"images/HotelMumbaiPosterJumbo.jpg",
+        trailer:"https://www.youtube.com/embed/gVQpbp54ljA"
+    },
+    {   
+        id:"cm",
+        title:"Captain Marvel",
+        released:"08.03.",
+        lenght: "2h 08m",
+        rating: "PG-13",
+        criticScore: "7/10",
+        description:`The story follows Carol Danvers as she becomes one of the universe's most powerful heroes when Earth is caught 
+        in the middle of a galactic war between two alien races. Set in the 1990s,
+         Captain Marvel is an all-new adventure from a previously unseen period in
+          the history of the Marvel Cinematic Universe.`,
+        poster:"images/CaptainMarvelPoster.jpeg",
+        rtLink:"https://www.rottentomatoes.com/m/captain_marvel",
+        jumboPoster:"images/CaptainMarvelPosterJumbo.jpg",
+        trailer:"https://www.youtube.com/embed/0LHxvxdRnYc"
+    },
+    {   
+        id:"us",
+        title:"Us",
+        released:"22.03.",
+        lenght: "2h 0m",
+        rating: "R",
+        criticScore: "8/10",
+        description:`Haunted by an unexplainable and unresolved trauma
+         from her past and compounded by a string of eerie coincidences,
+          Adelaide feels her paranoia elevate to high-alert as she grows increasingly certain that
+           something bad is going to befall her family. After spending a tense beach day with their friends, the Tylers
+            (Emmy winner Elisabeth Moss, Tim Heidecker, 
+            Cali Sheldon, Noelle Sheldon), Adelaide and her family return to their vacation home. When darkness falls.`,
+        poster:"images/UsPoster.jpeg",
+        rtLink:"https://www.rottentomatoes.com/m/us_2019",
+        jumboPoster:"images/UsPosterJumbo.jpg",
+        trailer:"https://www.youtube.com/embed/1tzFRIQfwXg"
+    },
+    {   
+        id:"bk",
+        title:"Booksmart",
+        released:"24.05.",
+        lenght: "1h 45m",
+        rating: "R",
+        criticScore: "7.5/10",
+        description:`The story follows Dever and Feldstein's characters,
+         two academic superstars and best friends who, on the eve of their high school graduation,
+          suddenly realize that they should have worked less and played more.
+         Determined never to fall short of their peers,
+          the girls set out on a mission to cram four years of fun into one night.`,
+        poster:"images/BooksmartPoster.jpg",
+        rtLink:"https://www.rottentomatoes.com/m/booksmart",
+        jumboPoster:"images/BooksmartPosterJumbo.jpg",
+        trailer:"https://www.youtube.com/embed/Uhd3lo_IWJc"
+    },
+    {   
+        id:"dj",
+        title:"A Dog's Journey",
+        released:"17.05.",
+        lenght: "1h 45m",
+        rating: "PG",
+        criticScore: "7/10",
+        description:`Bailey  is living the good life on the Michigan farm of his "boy,"
+         Ethan and Ethan's wife Hannah . 
+         He even has a new playmate: Ethan and Hannah's baby granddaughter, CJ.
+          The problem is that CJ's mom, Gloria (Betty Gilpin), decides to take CJ away.
+         As Bailey's soul prepares to leave this life for a new one, he makes a promise to Ethan to find CJ and protect her at any cost.`,
+        poster:"images/DogsJourneyPoster.jpg",
+        rtLink:"https://www.rottentomatoes.com/m/a_dogs_journey",
+        jumboPoster:"images/DogJourneyPosterJumbo.png",
+        trailer:"https://www.youtube.com/embed/A2FrrSyyKfA"
+    }
+
+
+
+
+];
